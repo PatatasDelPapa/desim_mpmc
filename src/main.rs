ca02da61bf019c628648721dfd310e6b2b0d8146
PatatasDelPapa <43626645+PatatasDelPapa@users.noreply.github.com>
2022-08-16 18:21:44 -0400

    Modeled the start of both producer and consumer doing the hack

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 use std::{cell::Cell, rc::Rc};
 
 use desim::{Simulation, Effect};
+use models::{producer, PassivatedList, consumer, Passivated};
 use state::State;
 
 mod models;
@@ -22,10 +23,37 @@ pub fn set_simulation() -> Simulation<Effect> {
     let shared_state = Rc::new(Cell::new(State::default()));
 
     let mut state = shared_state.take();
-    state.insert(0);
+    let count_key = state.insert(0);
+
+    let passivated_list = PassivatedList::default();
+    let passivated_list = state.insert(passivated_list);
 
     let producer_key_1 = state.insert(usize::MAX);
     let consumer_key_1 = state.insert(usize::MAX);
+
+    let producer = simulation.create_process(producer(
+        Rc::clone(&shared_state),
+        passivated_list,
+        count_key,
+        producer_key_1,
+    ));
+
+    let consumer = simulation.create_process(consumer(
+        Rc::clone(&shared_state),
+        passivated_list,
+        count_key,
+        consumer_key_1,
+    ));
+    
+
+    let producer_key_1 = state.get_mut(producer_key_1).unwrap();
+    *producer_key_1 = producer;
+
+    let consumer_key_1 = state.get_mut(consumer_key_1).unwrap();
+    *consumer_key_1 = consumer;
+
+    let passivated_list = state.get_mut(passivated_list).unwrap();
+    *passivated_list = PassivatedList::new(vec![(producer, Passivated::False)], vec![(consumer, Passivated::False)]);
 
     shared_state.set(state);
     simulation
--- src/models.rs
+++ src/models.rs
@@ -4,27 +4,97 @@ use desim::{SimGen, Effect, ProcessId};
 
 use crate::state::{State, StateKey};
 
+#[derive(Default, Debug)]
 pub struct PassivatedList {
     producers: Vec<(ProcessId, Passivated)>,
     consumers: Vec<(ProcessId, Passivated)>,
 }
 
-#[derive(PartialEq, Clone, Copy)]
-enum Passivated {
+impl PassivatedList {
+    pub fn new(producers: Vec<(ProcessId, Passivated)>, consumers: Vec<(ProcessId, Passivated)>) -> Self {
+        Self {
+            producers,
+            consumers,
+        }
+    }
+}
+
+#[derive(PartialEq, Clone, Copy, Debug)]
+pub enum Passivated {
     True,
     False,
-    Warned,
+    Hack,
 }
 
-pub fn producer(shared_state: Rc<Cell<State>>, passivated_key: StateKey<PassivatedList>, consumer_key: StateKey<Vec<usize>>, count_key: StateKey<usize>, self_id: StateKey<usize>) -> Box<SimGen<Effect>> {
-    let mut state = shared_state.take();
-    
+pub fn producer(shared_state: Rc<Cell<State>>, passivated_key: StateKey<PassivatedList>, count_key: StateKey<usize>, self_id: StateKey<usize>) -> Box<SimGen<Effect>> {
+    let state = shared_state.take();
     let self_id = *state.get(self_id).unwrap();
-    
     shared_state.set(state);
-    todo!()
+    Box::new(move |_| {
+        loop {
+            // Take the state out of the Rc<Cell<State>>
+            let mut state = shared_state.take();
+
+            //////////////////////////////////////
+            // -------- BEGIN HACK CHECK ---------
+            //////////////////////////////////////
+            let passivated_list = &mut state.get_mut(passivated_key).unwrap();
+            let maybe_passivated = passivated_list.producers.iter_mut().find(|(_, state)| *state == Passivated::Hack);
+            if let Some(&mut (passivated_id, _)) = maybe_passivated {
+                // Convert self to HACK and send Activate
+                let self_state = passivated_list.consumers.iter_mut().find(|(id, _)| *id == self_id).map(|(_, state)| state).unwrap();
+                *self_state = Passivated::Hack;
+
+                shared_state.set(state);
+                yield Effect::Event { time: 0.0, process: passivated_id };
+
+                let mut state = shared_state.take();
+                let passivated_list = &mut state.get_mut(passivated_key).unwrap();
+                let self_state = passivated_list.consumers.iter_mut().find(|(id, _)| *id == self_id).map(|(_, state)| state).unwrap();
+                *self_state = Passivated::False;
+
+            } else {
+                shared_state.set(state);
+            }
+            ////////////////////////////////////
+            // -------- END HACK CHECK ---------
+            ////////////////////////////////////
+        }
+    })
 }
 
-pub fn consumer(shared_state: Rc<Cell<State>>, passivated_key: StateKey<PassivatedList>, consumer_key: StateKey<Vec<usize>>, count_key: StateKey<usize>, self_id: StateKey<usize>) -> Box<SimGen<Effect>> {
-    todo!()
+pub fn consumer(shared_state: Rc<Cell<State>>, passivated_key: StateKey<PassivatedList>, count_key: StateKey<usize>, self_id: StateKey<usize>) -> Box<SimGen<Effect>> {
+    let state = shared_state.take();
+    let self_id = *state.get(self_id).unwrap();
+    shared_state.set(state);
+    Box::new(move |_| {
+        loop {
+            // Take the state out of the Rc<Cell<State>>
+            let mut state = shared_state.take();
+
+            //////////////////////////////////////
+            // -------- BEGIN HACK CHECK ---------
+            //////////////////////////////////////
+            let passivated_list = &mut state.get_mut(passivated_key).unwrap();
+            let maybe_passivated = passivated_list.consumers.iter_mut().find(|(_, state)| *state == Passivated::Hack);
+            if let Some(&mut (passivated_id, _)) = maybe_passivated {
+                // Convert self to HACK and send Activate
+                let self_state = passivated_list.producers.iter_mut().find(|(id, _)| *id == self_id).map(|(_, state)| state).unwrap();
+                *self_state = Passivated::Hack;
+                shared_state.set(state);
+                yield Effect::Event { time: 0.0, process: passivated_id };
+                let mut state = shared_state.take();
+                let passivated_list = &mut state.get_mut(passivated_key).unwrap();
+                let self_state = passivated_list.producers.iter_mut().find(|(id, _)| *id == self_id).map(|(_, state)| state).unwrap();
+                *self_state = Passivated::False;
+            } else {
+                shared_state.set(state);
+            }
+            ////////////////////////////////////
+            // -------- END HACK CHECK ---------
+            ////////////////////////////////////
+
+        }
+    })
 }
+
